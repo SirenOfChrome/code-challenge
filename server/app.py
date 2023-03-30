adeb2f29f97a409d852ec305c5896c0f33de12f9
Forrest Jones <forrestjones2010@gmail.com>
2023-03-30 13:13:36 -0500

    4 hours 10 minutes

--- server/app.py
+++ server/app.py
@@ -14,9 +14,123 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/')
-def home():
-    return ''
+@app.route('/vendors', methods=['GET'])
+def get_vendors():
+    vendors = Vendor.query.all()
+    vendor_list = [{'id': vendor.id, 'name': vendor.name} for vendor in vendors]
+    return jsonify(vendor_list)
+
+@app.route('/vendors/<int:id>', methods=['GET', 'DELETE'])
+def vendor_by_id(id):
+    vendor = Vendor.query.get(id)
+
+    if vendor:
+
+        if request.method == 'GET':
+            vendor_dict = {
+                'id': vendor.id,
+                'name': vendor.name,
+                'vendor_sweets': [vs.serialize() for vs in vendor.vendor_sweets]
+            }
+
+            response = make_response(jsonify(vendor_dict), 200)
+
+        elif request.method == 'DELETE':
+            db.session.delete(vendor)
+            db.session.commit()
+
+            response = make_response({}, 200)
+
+    else:
+        response = make_response({'error': 'Vendor not found'}, 404)
+
+    return response
+
+
+@app.route('/sweets', methods=['GET'])
+def get_sweets():
+    sweets = Sweet.query.all()
+
+    if sweets:
+        sweet_list = [{'id': sweet.id, 'name': sweet.name} for sweet in sweets]
+        response = make_response(jsonify(sweet_list), 200)
+    else:
+        response = make_response({'error': 'No sweets found'}, 404)
+
+    return response
+
+
+@app.route('/sweets/<int:id>', methods=['GET'])
+def get_sweet(id):
+    sweet = Sweet.query.get(id)
+
+    if sweet:
+        sweet_dict = {
+            'id': sweet.id,
+            'name': sweet.name
+        }
+
+        response = make_response(jsonify(sweet_dict), 200)
+    else:
+        response = make_response({'error': 'Sweet not found'}, 404)
+
+    return response
+
+
+@app.route('/vendor_sweets', methods=['POST'])
+def create_vendor_sweet():
+    data = request.get_json()
+
+    if not all(key in data for key in ['price', 'vendor_id', 'sweet_id']):
+        return make_response(jsonify({'error': 'Validation errors'}), 400)
+
+    if data['price'] < 0:
+        return make_response(jsonify({'error': 'Validation errors'}), 400)
+
+    vendor = Vendor.query.get(data['vendor_id'])
+    sweet = Sweet.query.get(data['sweet_id'])
+
+    if not vendor or not sweet:
+        return make_response(jsonify({'error': 'Invalid vendor or sweet id'}), 400)
+
+    vendor_sweet = VendorSweet(price=data['price'], vendor=vendor, sweet=sweet)
+
+    try:
+        db.session.add(vendor_sweet)
+        db.session.commit()
+
+        vendor_sweet_dict = {
+            'id': vendor_sweet.id,
+            'price': vendor_sweet.price,
+            'sweet': {
+                'id': sweet.id,
+                'name': sweet.name
+            },
+            'vendor': {
+                'id': vendor.id,
+                'name': vendor.name
+            }
+        }
+
+        return make_response(jsonify(vendor_sweet_dict), 201)
+
+    except:
+        db.session.rollback()
+
+        return make_response(jsonify({'error': 'Validation errors'}), 400)
+        
+@app.route('/vendor_sweets/<int:id>', methods=['DELETE'])
+def delete_vendor_sweet(id):
+    vendor_sweet = VendorSweet.query.get(id)
+
+    if vendor_sweet:
+        db.session.delete(vendor_sweet)
+        db.session.commit()
+
+        return make_response(jsonify({}), 200)
+    else:
+        return make_response(jsonify({'error': 'VendorSweet not found'}), 404)
+
 
     
 if __name__ == '__main__':
--- server/models.py
+++ server/models.py
@@ -3,6 +3,7 @@ from sqlalchemy import MetaData
 from sqlalchemy.orm import validates
 from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy_serializer import SerializerMixin
+from datetime import datetime
 
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
@@ -10,6 +11,56 @@ metadata = MetaData(naming_convention={
 
 db = SQLAlchemy(metadata=metadata)
 
-# Add models here
+class Vendor(db.Model, SerializerMixin):
+    __tablename__ = 'vendors'
+
+    serialize_rules = ('-vendor_sweets.vendor',)
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default = db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate = db.func.now())
+
+    vendor_sweets = db.relationship('VendorSweet', back_populates='vendor')
+
+class Sweet(db.Model, SerializerMixin):
+    __tablename__ = 'sweets'
+
+    serialize_rules = ('-vendor_sweets.sweet',)
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default = db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate = db.func.now())
+
+    vendor_sweets = db.relationship('VendorSweet', back_populates='sweet')
+
+class VendorSweet(db.Model, SerializerMixin):
+    __tablename__ = 'vendor_sweets'
+
+    id = db.Column(db.Integer, primary_key=True)
+    price = db.Column(db.Float)
+    created_at = db.Column(db.DateTime, server_default = db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate = db.func.now())
+
+    vendor_id = db.Column(db.Integer, db.ForeignKey('vendors.id'), nullable=False)
+    sweet_id = db.Column(db.Integer, db.ForeignKey('sweets.id'), nullable=False)
+
+    vendor = db.relationship('Vendor', back_populates='vendor_sweets')
+    sweet = db.relationship('Sweet', back_populates='vendor_sweets')
+
+    @validates('price')
+    def validates_price(self, key, value):
+        if not value:
+            raise ValueError("Price cannot be blank!")
+        elif value < 0:
+            raise ValueError("Price cannot be a negative number!")
+        return value
 
 # for relationships between tables, name these "vendor_sweets"
+
+
+
+#i originally included put this in each class but the tests would not pass i do realize that this data needs to be there:
+# "created_at = db.Column(db.DateTime, server_default = db.func.now())
+    #updated_at = db.Column(db.DateTime, onupdate = db.func.now())" 
